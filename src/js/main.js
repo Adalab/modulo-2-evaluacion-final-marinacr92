981f5093f3a4b415d5deb77534cc7d03daacc771
marinacr92 <marinacaro92@gmail.com>
2023-02-23 19:29:00 +0100

    Add event to enter key

--- src/js/main.js
+++ src/js/main.js
@@ -7,39 +7,46 @@ const btnReset = document.querySelector('.js-btn-reset');
 const articleSearch = document.querySelector('.js-article-search-list');
 const inputSearch = document.querySelector('.js-input-search');
 
-function renderDrink(drink){
-    const drinkTagName = document.createElement('h3');
-    const drinkName = document.createTextNode(drink.strDrink);
-    drinkTagName.appendChild(drinkName);
-    articleSearch.appendChild(drinkTagName);
-    const drinkTagImg = document.createElement('img');
-    drinkTagImg.setAttribute('src', drink.strDrinkThumb);
-    drinkTagImg.style = 'width: 200px; height: 200px';
-    articleSearch.appendChild(drinkTagImg);
+function renderDrink(drink) {
+  const drinkTagName = document.createElement('h3');
+  const drinkName = document.createTextNode(drink.strDrink);
+  drinkTagName.appendChild(drinkName);
+  articleSearch.appendChild(drinkTagName);
+  const drinkTagImg = document.createElement('img');
+  drinkTagImg.setAttribute('src', drink.strDrinkThumb);
+  drinkTagImg.style = 'width: 200px; height: 200px';
+  articleSearch.appendChild(drinkTagImg);
 }
 
 fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
- .then((response) => response.json())
- .then((data) => {
-    console.log(data.drinks)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data.drinks);
     for (const drink of data.drinks) {
-        renderDrink(drink);  
+      renderDrink(drink);
     }
-});
+  });
 
+function handleEnterKey(ev) {
+  if (ev.keyCode === 13) {
+    handleClickSearch(ev);
+  }
+}
 
-
-function handleClickSearch(ev){
-    ev.preventDefault();
-    articleSearch.innerHTML = '';
-    const inputSearchValue = inputSearch.value;
-    fetch (`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputSearchValue}`)
-    .then ((response) => response.json())
-    .then ((data) => {
-        for (const drink of data.drinks) {
-            renderDrink(drink)
-        }
-    })
+function handleClickSearch(ev) {
+  ev.preventDefault();
+  articleSearch.innerHTML = '';
+  const inputSearchValue = inputSearch.value;
+  fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputSearchValue}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      for (const drink of data.drinks) {
+        renderDrink(drink);
+      }
+    });
 }
 
 btnSearch.addEventListener('click', handleClickSearch);
+document.addEventListener('keypress', handleEnterKey);
